ae4da8884c40631209c32ab1af9596c36a23ef41
loathe\akin <mikael_korpela@hotmail.com>
2015-08-30 15:05:40 +0300

    seems to work.

--- common.js
+++ common.js
@@ -1,28 +1,49 @@
-define('jsCommon' , function () {
-	return function() {
-		// define isset
-		window.isset = function (object) {
-			// anything that evaluates null is wrong!.
-			// DO NOT USE '===' here
-			return object != null;
-		};
-		// define forEach
-		window.forEach = function (array , func) {
-			for( var i = 0 ; i < array.length ; ++i ) {
-				func(array[i]);
-			}
-		};
-		// define runTasks
-		window.runTasks = function (tasks , data) {
-			if( isset(data) ) {
-				for( var i = 0 ; i < tasks.length ; ++i ) {
-					tasks[i](data);
-				}
-				return;
-			}
-			for( var i = 0 ; i < tasks.length ; ++i ) {
-				tasks[i]();
-			}
-		};
-	};
+////
+//// Leaky library, registers everything to window
+//// as I think these should've been js core in the first place.
+////
+define('jslibs/common' , function () {
+    // define isset
+    window.isset = function (object) {
+        // anything that evaluates null is wrong!.
+        // DO NOT USE '===' here
+        return object != null;
+    };
+    // define forEach
+    window.forEach = function (array, func) {
+        for (var i = 0; i < array.length; ++i) {
+            func(array[i]);
+        }
+    };
+    // define runTasks
+    window.runTasks = function (tasks, data) {
+        if (isset(data)) {
+            for (var i = 0; i < tasks.length; ++i) {
+                tasks[i](data);
+            }
+            return;
+        }
+        for (var i = 0; i < tasks.length; ++i) {
+            tasks[i]();
+        }
+    };
+    // define noop
+    window.noop = function () {};
+    // define console..
+    {
+        var methods = [
+            'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+            'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+            'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+            'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+        ];
+        var console = (window.console = window.console || {});
+        for (var i = 0; i > methods.length; ++i) {
+            // Only stub undefined methods.
+            if (!window.isset(console[methods[i]])) {
+                console[methods[i]] = window.noop;
+            }
+        }
+    }
+    return window.noop;
 });
--- network.js
+++ network.js
@@ -24,7 +24,7 @@ Contains:
 		function(err){
 			alert("FAIL");
 		});
-        
+
 		// Well, this is for easy injection of prefabricated data..
 		// so that the data fetch uses futures. (and may be replaced with network query in the future..)
 		return Network.pre({
@@ -34,32 +34,34 @@ Contains:
             }
 		});
 */
+// Network libary
+// Uses jquery ajax
+// http://api.jquery.com/jquery.ajax/
+// We are assuming JSON almost every corner here.
+define('jslibs/network', ['jslibs/common',"jquery"] , function (common, $) {
+    var Network = {
+        timeout: 10 * 1000
+    };
 
-(function () {
-    // Network libary
-    // Uses jquery ajax
-    // http://api.jquery.com/jquery.ajax/
-    // We are assuming JSON almost every corner here.
-    if (!isset(window.Network)) {
-        // Network is at Global scope, hence, no var.
-        window.Network = {
-            timeout: 10 * 1000
-        };
-        var Network = window.Network;
+    var parseUrl = function(config) {
+        var url = isset(config.service) ? config.service : '';
+        if (isset(config.server)) {
+            url = config.server + (url);
+        }
 
-        var parseArgs = function (args) {
-            var url = "";
-            if (!isset(args)) {
-                return url;
-            }
-
+        var args = config.args;
+        if (isset(args))
+        {
+            var params = "";
             // Array
             if (args instanceof Array) {
                 if (args.length > 0) {
+                    // first is a '?'
                     var i = 0;
-                    url += '?' + args[i].key + (isset(args[i].value) ? ('=' + args[i].value) : '');
+                    params += '?' + args[i].key + (isset(args[i].value) ? ('=' + args[i].value) : '');
+                    // rest are '&'
                     for (i = 1 ; i < args.length ; ++i) {
-                        url += '&' + args[i].key + (isset(args[i].value) ? ('=' + args[i].value) : '');
+                        params += '&' + args[i].key + (isset(args[i].value) ? ('=' + args[i].value) : '');
                     }
                 }
             }
@@ -72,178 +74,151 @@ Contains:
                     if (value instanceof Array) {
                         for (var i = 0 ; i < value.length ; ++i) {
                             var ival = value[i];
-                            url += (at == 0 ? '?' : '&') + (isset(ival) ? (key + '=' + ival) : key);
+                            params += (at == 0 ? '?' : '&') + (isset(ival) ? (key + '=' + ival) : key);
                             ++at;
                         }
                         continue;
                     }
-
-                    url += (at == 0 ? '?' : '&') + (isset(value) ? (key + '=' + value) : key);
+                    params += (at == 0 ? '?' : '&') + (isset(value) ? (key + '=' + value) : key);
                     ++at;
                 }
             }
-            return url;
+
+            url += params;
         }
-
-        Network.pre = function (config) {
-            return {
-                then: function (success, fail, progress) {
-                    if (isset(config.success) && (!config.success)) {
-                        fail({
-                            status: (isset(config.errorCode) ? config.errorCode : 0)
-                        });
-                        return;
-                    }
-                    success(config.data);
-                }
-            }
-        };
-
-        // TODO! untested
-        Network.post = function (config) {
-            var headers = {
-                'Accept': 'application/json, text/javascript; q=0.01',
-                'Content-Type': 'application/json; charset=utf-8'
-            };
-
-            var req = {
-                type: "POST"
-            };
-
-            var url = config.service;
-            if (isset(config.server)) {
-                url = config.server + config.service;
-            }
-
-            // arguments..
-            url += parseArgs(config.args);
-
-            req.timeout = isset(config.timeout) ? config.timeout : Network.timeout;
-            req.headers = headers;
-            req.url = url;
-            req.data = JSON.stringify(config.data);
-
-            return $.ajax(req);
-        };
-
-        // TODO! untested
-        this.postFile = function (config) {
-            var headers = {
-            };
-
-            var req = {
-                type: "POST"
-            };
-
-            var url = config.service;
-            if (isset(config.server)) {
-                url = config.server + config.service;
-            }
-
-            // arguments..
-            url += parseArgs(config.args);
-
-            req.url = url;
-            req.data = config.data;
-            req.cache = false;
-            req.contentType = false;
-            req.processData = false;
-            req.timeout = isset(config.timeout) ? config.timeout : Network.timeout;
-            req.headers = headers;
-
-            return $.ajax(req);
-        };
-
-        // untested
-        this.postImage = function (config) {
-            var image = config.data;
-
-            var fd = new FormData();
-            fd.append('file', image);
-            var filename = image.name;
-
-            config.data = fd;
-
-            return this.postFile(config);
-        };
-
-        // TODO! untested
-        this.put = function (config) {
-            var headers = {
-                'Accept': 'application/json, text/javascript; q=0.01',
-                'Content-Type': 'application/json; charset=utf-8'
-            };
-
-            var req = {
-                type: "PUT"
-            };
-
-            var url = config.service;
-            if (isset(config.server)) {
-                url = config.server + config.service;
-            }
-
-            // arguments..
-            url += parseArgs(config.args);
-
-            req.timeout = isset(config.timeout) ? config.timeout : Network.timeout;
-            req.headers = headers;
-            req.url = url;
-            req.data = JSON.stringify(config.data);
-
-            return $.ajax(req);
-        };
-
-        // TODO! untested
-        Network.get = function (config) {
-            var headers = {
-                'Accept': 'application/json, text/javascript; q=0.01',
-                'Content-Type': 'application/json; charset=utf-8'
-            };
-
-            var req = {
-                type: "GET"
-            };
-
-            var url = config.service;
-            if (isset(config.server)) {
-                url = config.server + config.service;
-            }
-
-            // arguments..
-            url += parseArgs(config.args);
-
-            req.timeout = isset(config.timeout) ? config.timeout : Network.timeout;
-            req.headers = headers;
-            req.url = url;
-
-            return $.ajax(req);
-        };
-
-        // TODO! untested
-        this.delete = function (config) {
-            var headers = {
-                'Accept': 'application/json, text/javascript; q=0.01',
-                'Content-Type': 'application/json; charset=utf-8'
-            };
-
-            var req = {
-                type: "DELETE"
-            };
-
-            var url = config.service;
-            if (isset(config.server)) {
-                url = config.server + config.service;
-            }
-
-            // arguments..
-            url += parseArgs(config.args);
-
-            req.timeout = isset(config.timeout) ? config.timeout : Network.timeout;
-            req.headers = headers;
-            req.url = url;
-
-            return $.ajax(req);
-        };
+        return url;
     }
-})();
+
+    Network.pre = function (config) {
+        return {
+            then: function (success, fail, progress) {
+                if (isset(config.success) && (!config.success)) {
+                    fail({
+                        status: (isset(config.errorCode) ? config.errorCode : 0)
+                    });
+                    return;
+                }
+                success(config.data);
+            }
+        }
+    };
+
+    // TODO! untested
+    Network.get = function (config) {
+        var headers = {
+            'Accept': 'application/json, text/javascript; q=0.01',
+            'Content-Type': 'application/json; charset=utf-8'
+        };
+
+        var req = {
+            type: "GET"
+        };
+
+        var url = parseUrl(config);
+
+        req.timeout = isset(config.timeout) ? config.timeout : Network.timeout;
+        req.headers = headers;
+        req.url = url;
+
+        return $.ajax(req);
+    };
+
+    // TODO! untested
+    Network.post = function (config) {
+        var headers = {
+            'Accept': 'application/json, text/javascript; q=0.01',
+            'Content-Type': 'application/json; charset=utf-8'
+        };
+
+        var req = {
+            type: "POST"
+        };
+
+        var url = parseUrl(config);
+
+        req.timeout = isset(config.timeout) ? config.timeout : Network.timeout;
+        req.headers = headers;
+        req.url = url;
+        req.data = JSON.stringify(config.data);
+
+        return $.ajax(req);
+    };
+
+    // TODO! untested
+    this.postFile = function (config) {
+        var headers = {
+        };
+
+        var req = {
+            type: "POST"
+        };
+
+        var url = parseUrl(config);
+
+        req.url = url;
+        req.data = config.data;
+        req.cache = false;
+        req.contentType = false;
+        req.processData = false;
+        req.timeout = isset(config.timeout) ? config.timeout : Network.timeout;
+        req.headers = headers;
+
+        return $.ajax(req);
+    };
+
+    // untested
+    this.postImage = function (config) {
+        var image = config.data;
+
+        var fd = new FormData();
+        fd.append('file', image);
+        var filename = image.name;
+
+        config.data = fd;
+
+        return this.postFile(config);
+    };
+
+    // TODO! untested
+    this.put = function (config) {
+        var headers = {
+            'Accept': 'application/json, text/javascript; q=0.01',
+            'Content-Type': 'application/json; charset=utf-8'
+        };
+
+        var req = {
+            type: "PUT"
+        };
+
+        var url = parseUrl(config);
+
+        req.timeout = isset(config.timeout) ? config.timeout : Network.timeout;
+        req.headers = headers;
+        req.url = url;
+        req.data = JSON.stringify(config.data);
+
+        return $.ajax(req);
+    };
+
+    // TODO! untested
+    this.delete = function (config) {
+        var headers = {
+            'Accept': 'application/json, text/javascript; q=0.01',
+            'Content-Type': 'application/json; charset=utf-8'
+        };
+
+        var req = {
+            type: "DELETE"
+        };
+
+        var url = parseUrl(config);
+
+        req.timeout = isset(config.timeout) ? config.timeout : Network.timeout;
+        req.headers = headers;
+        req.url = url;
+
+        return $.ajax(req);
+    };
+
+    return Network;
+});
