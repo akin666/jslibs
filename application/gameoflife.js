a1bf9ba68b72a3b7e248e102ea0a8e32b30fd985
mikael korpela <mikael.korpela@zerobit.org>
2015-09-01 19:04:14 +0300

    factory not needed

--- application/gameoflife.js
+++ application/gameoflife.js
@@ -3,9 +3,20 @@
  */
 define(function () {
     function Application(config){
-        this.element = config.element;
+        var element = config.element;
+        this.element = element[0];
 
         this.element.innerHTML = "Gameoflife";
+
+        this.width = element.attr("data-width");
+        this.height = element.attr("data-height");
+
+        /*
+        1) any live cell with fewer than 2 neighbours dies.
+        2) any live cell with 2 or 3 neighbours lives on
+        3) any live cell with more than 3 neighbours dies
+        4) any dead cell with 3 neighbours becomes live cell
+         */
 
         return this;
     }
