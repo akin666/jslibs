804173d800c019c988246b0cc7132c7287217fb3
mikael korpela <mikael.korpela@zerobit.org>
2015-12-08 00:56:05 +0200

    no message

--- data.js
+++ data.js
@@ -7,7 +7,8 @@ define(["three"],
         class Data {
             _readTyped(item) {
                 switch(item.type) {
-                    case 'position' : {
+                    case 'position' :
+                    case 'vector' : {
                         // ?x,?y,?z
                         var x = parseFloat(item.x);
                         var y = parseFloat(item.y);
@@ -15,9 +16,97 @@ define(["three"],
 
                         if( isNaN(x) ) x = 0.0;
                         if( isNaN(y) ) y = 0.0;
-                        if( isNaN(z) ) z = 0.0;
+                        if( isNaN(z) ) {
+                            return new THREE.Vector2(x,y);
+                        }
 
                         return new THREE.Vector3(x,y,z);
+                    }
+                    case 'euler' : {
+                        // ?x,?y,?z
+                        var x = parseFloat(item.x);
+                        var y = parseFloat(item.y);
+                        var z = parseFloat(item.z);
+                        var order = item.order;
+
+                        if( isNaN(x) ) x = 0.0;
+                        if( isNaN(y) ) y = 0.0;
+                        if( isNaN(z) ) z = 0.0;
+                        if( order === undefined || order === null ) {
+                            order = 'XYZ';
+                        }
+
+                        order = order.toUpperCase();
+
+                        return new THREE.Euler(x,y,z,order);
+                    }
+                    case 'quaternion' : {
+                        // ?x,?y,?z
+                        var x = parseFloat(item.x);
+                        var y = parseFloat(item.y);
+                        var z = parseFloat(item.z);
+                        var w = parseFloat(item.w);
+
+                        if( isNaN(x) || isNaN(y) || isNaN(z) || isNaN(w) ) {
+                            throw "quaternion needs 4 values";
+                        }
+
+                        return new THREE.Quaternion(x,y,z,w);
+                    }
+                    case 'mat4' :
+                    case 'matrix4' :
+                    case 'matrix4x4' : {
+                        var data = [];
+                        if( item.data == null || !(item.data instanceof Array) ) {
+                            return new THREE.Matrix4();
+                        }
+
+                        for( var i = 0 ; i < item.data.length ; ++i ) {
+                            var num = parseFloat(item.data[i]);
+                            if(isNaN(num)) {
+                                throw "matrix4 needs number values";
+                            }
+                            data.push(num);
+                        }
+
+
+                        if( data.length === 16 ) {
+                            var t = new THREE.Matrix4();
+                            t.fromArray(data);
+                            return t;
+                        }
+
+                        throw "matrix4 needs 16 values";
+                    }
+                    case 'color' : {
+                        // ?r,?g,?b
+                        var r = parseFloat(item.red);
+                        var g = parseFloat(item.green);
+                        var b = parseFloat(item.blue);
+
+                        if( isNaN(r) ) r = 0.0;
+                        if( isNaN(g) ) g = 0.0;
+                        if( isNaN(b) ) b = 0.0;
+
+                        return new THREE.Color(r,g,b);
+                    }
+                    case 'rectangle' :
+                    case 'box' : {
+                        // ?r,?g,?b
+                        var min = this._readTyped(item.min);
+                        var max = this._readTyped(item.max);
+
+                        if( min instanceof THREE.Vector2 ) {
+                            return new THREE.Box2( min , max );
+                        }
+                        return new THREE.Box3( min , max );
+                    }
+                    case 'line' : {
+                        // ?r,?g,?b
+                        var begin = this._readTyped(item.begin);
+                        var end = this._readTyped(item.end);
+
+                        return new THREE.Line3( begin , end );
                     }
                     default:
                         break;
--- data.test.js
+++ data.test.js
@@ -12,18 +12,184 @@ define(["./data" , "three"],
                 assert.notEqual(target, undefined, "undefined test");
             },
             testReadPosition(assert) {
+
                 var target = new Data();
 
-                var vec3 = new THREE.Vector3(100,100,10);
+                // Vector3
+                (function(){
+                    var item = new THREE.Vector3(100,100,10);
+                    var content = target.read({
+                        type: "position",
+                        x: item.x,
+                        y: item.y,
+                        z: item.z
+                    });
+
+                    // content should be threejs vector3
+                    assert.ok( (content instanceof THREE.Vector3) && item.equals(content),"Position3 serialization.");
+                })();
+
+                // Vector3
+                (function(){
+                    var item = new THREE.Vector3(10,10,10);
+                    var content = target.read({
+                        type: "vector",
+                        x: item.x,
+                        y: item.y,
+                        z: item.z
+                    });
+
+                    // content should be threejs vector3
+                    assert.ok( (content instanceof THREE.Vector3) && item.equals(content),"Vector3 serialization.");
+                })();
+
+                // Vector2
+                (function(){
+                    var item = new THREE.Vector2(100,100);
+                    var content = target.read({
+                        type: "position",
+                        x: item.x,
+                        y: item.y
+                    });
+
+                    // content should be threejs vector2
+                    assert.ok( (content instanceof THREE.Vector2) && item.equals(content),"Position2 serialization.");
+                })();
+
+                // Vector3
+                (function(){
+                    var item = new THREE.Vector2(10,10);
+                    var content = target.read({
+                        type: "vector",
+                        x: item.x,
+                        y: item.y
+                    });
+
+                    // content should be threejs vector2
+                    assert.ok( (content instanceof THREE.Vector2) && item.equals(content),"Vector2 serialization.");
+                })();
+            },
+            testReadAngle(assert) {
+                var target = new Data();
+
+                // Euler type angles.
+                (function() {
+                    var item = new THREE.Euler(1, 1, 1, 'XYZ');
+                    var content = target.read({
+                        type: "euler",
+                        x: item.x,
+                        y: item.y,
+                        z: item.z,
+                        order: 'xyz'
+                    });
+
+                    // content should be threejs Euler
+                    assert.ok((content instanceof THREE.Euler) && item.equals(content), "Euler serialization.");
+                })();
+
+                // Quaternion type angles.
+                (function() {
+                    var item = new THREE.Quaternion(1, 1, 1, 1);
+                    var content = target.read({
+                        type: "quaternion",
+                        x: item.x,
+                        y: item.y,
+                        z: item.z,
+                        w: item.w
+                    });
+
+                    // content should be threejs Quaternion
+                    assert.ok((content instanceof THREE.Quaternion) && item.equals(content), "Quaternion serialization.");
+                })();
+            },
+            testReadMatrix(assert) {
+                var target = new Data();
+
+                // Mat4.
+                (function() {
+                    var item = new THREE.Matrix4();
+                    item.fromArray([1,2,4,5,5,6,7,8,9,10,11,12,13,14,15,16]);
+                    var content = target.read({
+                        type: "matrix4",
+                        data: item.toArray()
+                    });
+
+                    // content should be threejs Matrix4
+                    assert.ok((content instanceof THREE.Matrix4) && item.equals(content), "Matrix4 serialization.");
+                })();
+            },
+            testReadColor(assert) {
+                var target = new Data();
+
+                var item = new THREE.Color(1.0,1.0,1.0);
                 var content = target.read({
-                    "type": "position",
-                    "x": vec3.x,
-                    "y": vec3.y,
-                    "z": vec3.z
+                    type: "color",
+                    red: item.r,
+                    green: item.g,
+                    blue: item.b
                 });
 
-                // content should be threejs vector3
-                assert.ok( (content instanceof THREE.Vector3) && vec3.equals(content),"Position Vector3 serialization.");
+                // content should be threejs color
+                assert.ok( (content instanceof THREE.Color) && item.equals(content),"Color serialization.");
+            },
+            testReadShape(assert) {
+                var target = new Data();
+
+                // Box3
+                (function() {
+                    var min = new THREE.Vector3(0, 0, 0);
+                    var max = new THREE.Vector3(10, 10, 10);
+                    var content = target.read({
+                        type: "box",
+                        min: {
+                            type: "vector",
+                            x: min.x,
+                            y: min.y,
+                            z: min.z
+                        },
+                        max: {
+                            type: "vector",
+                            x: max.x,
+                            y: max.y,
+                            z: max.z
+                        }
+                    });
+
+                    // content should be threejs Box3
+                    assert.ok(
+                        (content instanceof THREE.Box3) &&
+                        min.equals(content.min) &&
+                        max.equals(content.max),
+                        "Box3 serialization.");
+                })();
+
+                // Line3
+                (function() {
+                    var begin = new THREE.Vector3(0, 0, 0);
+                    var end = new THREE.Vector3(10, 10, 10);
+                    var content = target.read({
+                        type: "line",
+                        begin: {
+                            type: "vector",
+                            x: begin.x,
+                            y: begin.y,
+                            z: begin.z
+                        },
+                        end: {
+                            type: "vector",
+                            x: end.x,
+                            y: end.y,
+                            z: end.z
+                        }
+                    });
+
+                    // content should be threejs Box3
+                    assert.ok(
+                        (content instanceof THREE.Line3) &&
+                        begin.equals(content.start) &&
+                        end.equals(content.end),
+                        "Line3 serialization.");
+                })();
             }
         };
 
